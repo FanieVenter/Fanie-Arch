3e62f106391bb6a0555be12620d0fa5bbb585bc1
Fanie Venter <georgesventer@gmail.com>
2024-05-23 18:32:11 +1200

    added basic functionalty to script

---
+++ start.sh
@@ -0,0 +1,49 @@
+
+cd ~
+nc=$(grep -c ^processor /proc/cpuinfo)
+TOTAL_MEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+if [[  $TOTAL_MEM -gt 8000000 ]]; then
+sudo sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$nc\"/g" /etc/makepkg.conf
+sudo sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g" /etc/makepkg.conf
+fi
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+sudo pacman -S --noconfirm --needed reflector rsync git curl dhclient
+sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+sudo sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+sudo sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+sudo pacman -Sy --noconfirm --needed
+gpu_type=$(lspci)
+if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
+    sudo pacman -S --noconfirm --needed nvidia
+	nvidia-xconfig
+elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
+    sudo pacman -S --noconfirm --needed xf86-video-amdgpu
+elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
+    sudo pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
+    sudo pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+fi
+
+git clone "https://aur.archlinux.org/yay.git"
+cd ~/yay
+makepkg -si --noconfirm
+cd ~
+# sed $INSTALL_TYPE is using install type to check for MINIMAL installation, if it's true, stop
+# stop the script and move on, not installing any more packages below that line
+
+
+yay -S --noconfirm --needed ~/Fanie-Arch/aur-apps.txt
+sudo pacman -S --noconfirm --needed ~/Fanie-Arch/pacman-apps.txt
+#clone and install dwm
+git clone https://github.com/FanieVenter/dwm
+cd dwm
+sudo make clean install
+cd ~ 
+#clone and install slstatus
+git clone https://github.com/FanieVenter/slstatus
+cd slstatus
+sudo make clean install
+cd ~
+#clone and install bash theme
+git clone
